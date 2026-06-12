fd5b7a9a30023b0f2721171ecf424b1860346dce
Alex Cseke <stefan.cseke@stud.ubbcluj.ro>
2026-06-12 10:50:35 +0300

    added scalers for the output of the regression model

---
+++ scalers.py
@@ -0,0 +1,38 @@
+import torch
+
+
+class TanhScaler:
+    def __init__(self, const):
+        self.constant = const
+
+    def scale(self, y_train):
+        return torch.tanh(y_train/self.constant)
+
+    def reverse(self, scaled_tensor):
+        return self.constant * torch.atanh(scaled_tensor)
+
+
+class AsinhScaler:
+    def __init__(self, const):
+        self.constant = const
+
+    def scale(self, y_train):
+        return torch.asinh(y_train/self.constant)
+
+    def reverse(self, scaled_tensor):
+        return self.constant * torch.sinh(scaled_tensor)
+
+
+class DistribScaler:
+    def __init__(self, mu, sigma):
+        self.mean = mu
+        self.std = sigma
+
+        if self.std == 0:
+            self.std = torch.tensor(1.0)
+
+    def scale(self, y_train):
+        return (y_train - self.mean)/self.std
+
+    def reverse(self, scaled_tensor):
+        return scaled_tensor*self.std + self.mean
